9bda7960f3039285d2609ce01b6c4e76a3009465
svaghela-2869 <sagarvaghela2869@gmail.com>
2023-07-03 17:51:09 +0530

    project structure small change.

--- specs/_projects/codezeroes/codezeroes_text_content_verification.spec.ts
+++ specs/_projects/codezeroes/codezeroes_text_content_verification.spec.ts
@@ -1,3 +1,3 @@
-import { spec_runner } from "../../lib/commom/generic_spec_runner";
+import { spec_runner } from "../../../lib/commom/generic_spec_runner";
 const yargs = require("yargs").argv;
 spec_runner(yargs, __dirname, __filename);
